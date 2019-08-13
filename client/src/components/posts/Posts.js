dc738b2a0219a346beb5ee651692a5cd91fdc8f9
Aishvarya verma <averma@clavax.us>
2019-08-13 11:29:29 +0530

    removed warnings

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
