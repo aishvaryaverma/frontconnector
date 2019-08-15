34ad8a683a539b7d2aa188e99064f48730958557
aishvaryaverma <vsonu9020@gmail.com>
2019-08-15 13:32:09 +0530

    Completed with deploy scripts

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDb = require('./config/db');
-// const cors = require('cors');
+const path = require('path');
 const app = express();
 
 // Connect Database
@@ -23,14 +23,22 @@ connectDb();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Get Request
-app.get('/', (req, res) => res.send('API Running'));
-
 // Routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+// Serving static assets in production
+
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // Listening for requests
 const PORT = process.env.PORT || 5000;
